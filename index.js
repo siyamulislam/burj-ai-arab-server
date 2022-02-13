1456658d15004bf3250305c35e1ec7f1d274e85d
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-13 21:09:57 +0600

    authorization verified

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { initializeApp } = require('firebase-admin/app');
 
 const port = 5000
 const app = express();
- app.use(cors());
- app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }))
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./burj-al-arab-2023-firebase-adminsdk-pooc5-c98f0cb4df.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 
 const userName = "arabian"
@@ -17,19 +26,40 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 client.connect(err => {
     const bookingCollection = client.db("burjAlArabDB").collection("bookings");
-    app.post('/addBooking',(req,res)=>{
+    app.post('/addBooking', (req, res) => {
         const newBooking = req.body;
         console.log(newBooking);
         bookingCollection.insertOne(newBooking)
-        .then(result=>res.send(result.acknowledged))
+            .then(result => res.send(result.acknowledged))
     })
 
-    app.get('/bookings',(req,res)=>{
-        console.log(req.headers.authorization);
-        bookingCollection.find({email:req.query.email})
-        .toArray((err,documents)=>{
-                res.send(documents);
-        })
+    app.get('/bookings', (req, res) => {
+        const bearer = req.headers.authorization;
+        if (bearer && bearer.startsWith("Bearer ")) {
+            const idToken = bearer.split(' ')[1]
+            // console.log(idToken);
+            admin.auth()
+                .verifyIdToken(idToken)
+                .then((decodedToken) => {
+                    const uid = decodedToken.uid;
+                    const emailToken = decodedToken.email;
+                    if (emailToken === req.query.email) {
+                        console.log(uid);
+                        bookingCollection.find({ email: req.query.email })
+                            .toArray((err, documents) => {
+                                res.send(documents);
+                            })
+                    }
+
+                    // ...
+                })
+                .catch((error) => {
+                    // Handle error
+                });
+
+
+        }
+
     })
 
 
@@ -43,9 +73,9 @@ client.connect(err => {
 
 
 
-app.get('/',(req,res)=>{ 
+app.get('/', (req, res) => {
     res.send("Hlw NODE JS");
-  })
+})
 
 
 
@@ -53,4 +83,4 @@ app.get('/',(req,res)=>{
 // app.listen(port);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+})
